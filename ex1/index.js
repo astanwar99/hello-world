cdeec3774988fc2f6f9749755837ca4b700bc8ea
astanwar99 <as.tanwar3198@gmail.com>
2019-09-11 23:22:32 +0530

    Exercise - 1

---
+++ ex1/index.js
@@ -0,0 +1,18 @@
+var rect = {
+    perimeter: (x, y) => (2*(x + y)),
+    area: (x, y) => (x * y)
+};
+
+function solveRect(l, b) {
+    console.log("l = " + l + ", b = " + b);
+
+    if(l <= 0 || b <= 0) {
+        console.log("Invalid input");
+    }else {
+        console.log("area: " + rect.area(l, b));
+        console.log("peri: " + rect.perimeter(l, b));
+    }
+}
+
+solveRect(3, 4);
+solveRect(3, 0);
