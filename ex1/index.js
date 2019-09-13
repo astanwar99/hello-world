efed01d46b09c16c6d563cf0dacba1b0fc92636e
Ashwani Singh Tanwar <30383121+astanwar99@users.noreply.github.com>
2019-09-13 20:00:37 +0530

    Implented callback and error handling

--- ex1/index.js
+++ ex1/index.js
@@ -2,14 +2,17 @@ var rect = require('./rectangle')
 
 function solveRect(l, b) {
     console.log("l = " + l + ", b = " + b);
-
-    if(l <= 0 || b <= 0) {
-        console.log("Invalid input");
-    }else {
-        console.log("area: " + rect.area(l, b));
-        console.log("peri: " + rect.perimeter(l, b));
-    }
+    rect(l, b, (err, rectangle) => {
+        if(err) {
+            console.log("ERROR: ", err.message);
+        }
+        else {
+            console.log("Area: " + rectangle.area());
+            console.log("Perimeter: " + rectangle.perimeter());
+        }
+    })
 }
 
+console.log("Asynchronously called instruction.");
 solveRect(3, 4);
 solveRect(3, 0);
