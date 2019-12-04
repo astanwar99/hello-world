4d561ab73b29013621b8d362cf7871e0432319f5
Ashwani Singh Tanwar <30383121+astanwar99@users.noreply.github.com>
2019-12-04 23:10:28 +0530

    Express sessions - 1

--- expressGenerator/conFusionServer/app.js
+++ expressGenerator/conFusionServer/app.js
@@ -3,6 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var session = require('express-session');
+var FileStore = require('session-file-store')(session);
 
 const mongoose = require('mongoose');
 
@@ -31,12 +33,19 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser('12345-67890-09876-54321'));//signed cookie
+//app.use(cookieParser('12345-67890-09876-54321'));//signed cookie
+app.use(session({
+  name: 'session-id',
+  secret: '12345-67890-09876-54321',
+  saveUninitialized: false,
+  resave: false,
+  store: new FileStore()
+}));
 
 function auth(req, res, next) {
-  console.log(req.signedCookies);
+  console.log(req.session);
 
-  if(!req.signedCookies.user) {
+  if(!req.session.user) {
     var authHeader = req.headers.authorization;
 
     if(!authHeader) {
@@ -46,13 +55,14 @@ function auth(req, res, next) {
       return next(err);
     }
 
-    var auth = new Buffer(authHeader.split(' ')[1], 'base64').toString().split(':');
+    var auth = new Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
     
     var username = auth[0];
     var password = auth[1];
 
     if(username === 'admin' && password === 'password'){
-      res.cookie('user', 'admin', {signed: true})
+      //res.cookie('user', 'admin', {signed: true})
+      req.session.user = 'admin';
       next();
     }else {
       var err = new Error('You are not authenticated!');
@@ -61,7 +71,7 @@ function auth(req, res, next) {
       return next(err);
     }
   }else {
-    if(req.signedCookies.user === 'admin') {
+    if(req.session.user === 'admin') {
       next();
     }else {
       var err = new Error('You are not authenticated!');
