c4b2e4a4fe26ac072489f1f78429a9d3c031625e
Ashwani Singh Tanwar <30383121+astanwar99@users.noreply.github.com>
2019-12-05 13:04:45 +0530

    Express session - 2

--- expressGenerator/conFusionServer/app.js
+++ expressGenerator/conFusionServer/app.js
@@ -42,40 +42,47 @@ app.use(session({
   store: new FileStore()
 }));
 
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
 function auth(req, res, next) {
   console.log(req.session);
 
   if(!req.session.user) {
-    var authHeader = req.headers.authorization;
+    var err = new Error('You are not authenticated!');
+    res.setHeader('WWW-Authenticate', 'Basic');
+    err.status = 403;
+    return next(err);
+    // var authHeader = req.headers.authorization;
 
-    if(!authHeader) {
-      var err = new Error('You are not authenticated!');
-      res.setHeader('WWW-Authenticate', 'Basic');
-      err.status = 401;
-      return next(err);
-    }
+    // if(!authHeader) {
+    //   var err = new Error('You are not authenticated!');
+    //   res.setHeader('WWW-Authenticate', 'Basic');
+    //   err.status = 401;
+    //   return next(err);
+    // }
 
-    var auth = new Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
+    // var auth = new Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
     
-    var username = auth[0];
-    var password = auth[1];
+    // var username = auth[0];
+    // var password = auth[1];
 
-    if(username === 'admin' && password === 'password'){
-      //res.cookie('user', 'admin', {signed: true})
-      req.session.user = 'admin';
-      next();
-    }else {
-      var err = new Error('You are not authenticated!');
-      res.setHeader('WWW-Authenticate', 'Basic');
-      err.status = 401;
-      return next(err);
-    }
+    // if(username === 'admin' && password === 'password'){
+    //   //res.cookie('user', 'admin', {signed: true})
+    //   req.session.user = 'admin';
+    //   next();
+    // }else {
+    //   var err = new Error('You are not authenticated!');
+    //   res.setHeader('WWW-Authenticate', 'Basic');
+    //   err.status = 401;
+    //   return next(err);
+    // }
   }else {
-    if(req.session.user === 'admin') {
+    if(req.session.user === 'authenticated') {
       next();
     }else {
       var err = new Error('You are not authenticated!');
-      err.status = 401;
+      err.status = 403;
       return next(err);
     }
   }
@@ -85,8 +92,7 @@ app.use(auth);
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+
 app.use('/dishes', dishRouter);
 app.use('/promotions', promoRouter);
 app.use('/leaders', leaderRouter);
